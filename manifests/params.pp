5c6bc6f0a13b0aea6d51ab5925800b9e378bf3d9
Leonardo Thibes <eu@leonardothibes.com>
2014-01-04 20:19:57 -0200

    removendo o pacote "php5-dev" da lista de pacotes a serem instalados

--- manifests/params.pp
+++ manifests/params.pp
@@ -6,11 +6,11 @@ class php::params
 	$etclink  = true
 	$modules  = []
 	$packages = [
+		'libapache2-mod-php5',
 		'libssl0.9.8',
 		'libssl-dev',
 		'php5',
 		'php5-cli',
-		'php5-dev',
 		'php-pear',
 	]
 	# Install options
