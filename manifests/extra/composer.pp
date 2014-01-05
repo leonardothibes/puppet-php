087b22286fc0f80735cf790be941403544e09e36
Leonardo Thibes <eu@leonardothibes.com>
2014-01-05 00:07:06 -0200

    implementando a instalacao do composer.phar

---
+++ manifests/extra/composer.pp
@@ -0,0 +1,14 @@
+class php::extra::composer
+{
+	package {'curl': ensure => present}
+	exec    {'composer-step-1':
+		command => 'curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin',
+		path    => "/usr/bin",
+		onlyif  => "test ! -f /usr/bin/composer.phar",
+		require => Package['curl'],
+	}
+	file {'/usr/bin/composer':
+		ensure => link,
+		target => '/usr/bin/composer.phar',
+	}
+}
--- tests/init.pp
+++ tests/init.pp
@@ -9,9 +9,8 @@
 # Learn more about module testing here: http://docs.puppetlabs.com/guides/tests_smoke.html
 #
 class {'php':
-	etclink => true,
 	modules => ['apc','pgsql','mysql','sybase','xdebug'],
-	extra   => ['code-sniffer','phing','phpdoc']
+	extra   => ['code-sniffer','phing','phpdoc','composer'],
 }
 
 #php::module::install{'apc':}
