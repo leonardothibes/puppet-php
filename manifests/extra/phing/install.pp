5dc76987d3a7272ef632394523649a3017421492
Leonardo Thibes <eu@leonardothibes.com>
2014-01-06 15:14:41 -0200

    fix install

--- manifests/extra/phing/install.pp
+++ manifests/extra/phing/install.pp
@@ -4,7 +4,7 @@ class php::extra::phing::install
 	exec {'phing-step-1':
 		command => 'pear channel-discover pear.phing.info',
 		path    => ['/bin','/usr/bin'],
-		onlyif  => 'test pear list-channels | grep pear.phing.info | wc -l == 0',
+		onlyif  => 'test ! -f /usr/bin/phing',
 		require => Class[php::extra::pear],
 		before  => Exec['phing-step-2'],
 	}
