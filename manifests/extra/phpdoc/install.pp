c780514f4f030bf5753bcf44cfcb1aa12471c0ad
Leonardo Thibes <eu@leonardothibes.com>
2014-01-06 18:25:33 -0200

    fix install

--- manifests/extra/phpdoc/install.pp
+++ manifests/extra/phpdoc/install.pp
@@ -2,16 +2,10 @@ class php::extra::phpdoc::install
 {
 	include php::extra::pear
 	php::module::install {'xsl':}
-	exec {'phpdoc-step-1':
-		command => 'pear channel-discover pear.phpdoc.org',
-		path    => '/usr/bin',
-		onlyif  => 'test ! -f /usr/bin/phpdoc',
-		require => Class[php::extra::pear],
-		before  => Exec['phpdoc-step-2'],
-	}
-	exec {'phpdoc-step-2':
+	exec {'php::extra::phpdoc::install':
 		command => 'pear install phpdoc/phpDocumentor-alpha',
 		path    => '/usr/bin',
 		onlyif  => 'test ! -f /usr/bin/phpdoc',
+		require => Class[php::extra::pear],
 	}
 }
