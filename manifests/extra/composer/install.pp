452348645b35cb16f0e8418251272320c923727f
Leonardo Thibes <eu@leonardothibes.com>
2014-02-25 15:30:01 -0300

    Incluindo os pacotes do PHP como dependencia em todos os modulos e extras #24

--- manifests/extra/composer/install.pp
+++ manifests/extra/composer/install.pp
@@ -1,13 +1,19 @@
 class php::extra::composer::install
 {
-	php::secure::install {'git':}
-	php::secure::install {'curl':}
-	php::secure::install {'php5-curl':}
+	include php::dependencies
+	$packages = ['git','curl','php5-curl']
+	package {'php::extra::composer::dependencies::packages':
+		ensure => present,
+		name   => $packages,
+	}
 	exec {'php::extra::composer::install':
 		command => 'curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/bin',
 		path    => "/usr/bin",
 		onlyif  => "test ! -f /usr/bin/composer.phar",
-		require => Package['curl'],
+		require => [
+			Package['curl'],
+			Class['php::dependencies'],
+		],
 	}
 	file {'/usr/bin/composer':
 		ensure => link,
