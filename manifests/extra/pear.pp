30cddcb2ad0dad3c1e6d70e8ac7f07470160a53c
Leonardo Thibes <eu@leonardothibes.com>
2014-02-25 16:28:38 -0300

    Incluindo os pacotes do PHP como dependencia em todos os modulos e extras #24

--- manifests/extra/pear.pp
+++ manifests/extra/pear.pp
@@ -1,19 +1,22 @@
-class php::extra::pear
+class php::extra::pear inherits php::dependencies
 {
 	exec {'php::extra::pear::step-1':
 		command => 'pear chanel-update pear.php.net',
 		path    => '/usr/bin',
 		before  => Exec['php::extra::pear::step-2'],
+		require => Class['php::dependencies'],
 	}
 
 	exec {'php::extra::pear::step-2':
 		command => 'pear config-set auto_discover 1',
 		path    => '/usr/bin',
+		require => Exec['php::extra::pear::step-1'],
 		before  => Exec['php::extra::pear::step-3'],
 	}
 
 	exec {'php::extra::pear::step-3':
 		command => 'pecl chanel-update pecl.php.net',
 		path    => '/usr/bin',
+		require => Exec['php::extra::pear::step-2'],
 	}
 }
