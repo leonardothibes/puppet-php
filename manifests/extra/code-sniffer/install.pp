0f44eb1c33dfb5253ecf6d3493eb356cfc329c9e
Leonardo Thibes <eu@leonardothibes.com>
2014-02-25 15:32:39 -0300

    Incluindo os pacotes do PHP como dependencia em todos os modulos e extras #24

--- manifests/extra/code-sniffer/install.pp
+++ manifests/extra/code-sniffer/install.pp
@@ -1,10 +1,14 @@
 class php::extra::code-sniffer::install
 {
+	include php::dependencies
 	include php::extra::pear
 	exec {'php::extra::code-sniffer::install':
 		command => 'pear install --alldeps PHP_CodeSniffer',
 		path    => '/usr/bin',
 		onlyif  => 'test ! -f /usr/bin/phpcs',
-		require => Class[php::extra::pear],
+		require => [
+			Class['php::dependencies'],
+			Class['php::extra::pear'],
+		],
 	}
 }
