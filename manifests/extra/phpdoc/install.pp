ee914c4c29d13567dc0551d140362bd65cb312b1
Leonardo Thibes <eu@leonardothibes.com>
2014-02-27 14:53:53 -0300

    Resolvendo conflito na instalacao de pacotes do PHP entre bibliotecas

--- manifests/extra/phpdoc/install.pp
+++ manifests/extra/phpdoc/install.pp
@@ -1,6 +1,12 @@
 class php::extra::phpdoc::install inherits php::dependencies
 {
-	php::module::install {'xsl':}
+	$packages = 'php::extra::phpdox::dependencies::packages'
+	if !defined(Package[$packages]) {
+		package {$packages:
+			ensure => present,
+			name   => 'php5-xsl',
+		}
+	}
 	wget::fetch {'php::extra::phpdoc::install':
 		source      => 'http://phpdoc.org/phpDocumentor.phar',
 		destination => '/usr/bin/phpDocumentor.phar',
--- manifests/extra/phpdox/install.pp
+++ manifests/extra/phpdox/install.pp
@@ -1,6 +1,12 @@
 class php::extra::phpdox::install inherits php::dependencies
 {
-	php::module::install {'xsl':}
+	$packages = 'php::extra::phpdox::dependencies::packages'
+	if !defined(Package[$packages]) {
+		package {$packages:
+			ensure => present,
+			name   => 'php5-xsl',
+		}
+	}
 	wget::fetch {'php::extra::phpdox::install':
 		source      => 'http://phpdox.de/releases/phpdox.phar',
 		destination => '/usr/bin/phpdox',
