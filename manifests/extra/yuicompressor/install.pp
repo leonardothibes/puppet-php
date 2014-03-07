f1f4f0b02d92c8d02405e3f293cd13015c01106b
Leonardo Thibes <eu@leonardothibes.com>
2014-03-07 11:47:11 -0300

    Implementando a instalacao do YuiCompressor #28

--- manifests/extra/yuicompressor/install.pp
+++ manifests/extra/yuicompressor/install.pp
@@ -4,7 +4,22 @@ class php::extra::yuicompressor::install inherits php::dependencies
 	$url = "https://github.com/yui/yuicompressor/releases/download/v${ver}/yuicompressor-${ver}.jar"
 	wget::fetch {'php::extra::yuicompressor::install':
 		source      => $url,
-		destination => '/usr/bin/yuicompressor',
+		destination => '/usr/bin/yuicompressor.jar',
 		chmod       => 0755,
+	}
+	$resources = 'php::extra::yuicompressor::dependencies'
+	if !defined(Package[$resources]) {
+		package {$resources:
+			ensure => present,
+			name   => 'openjdk-6-jre',
+		}
+	}
+	file {'php::extra::yuicompressor::alias-file':
+		ensure  => present,
+		owner   => root,
+		group   => root,
+		mode    => 0644,
+		path    => '/etc/profile.d/yuicompressor.sh',
+		content => "alias yuicompressor='java -jar /usr/bin/yuicompressor.jar'",
 	}
 }
--- manifests/extra/yuicompressor/purge.pp
+++ manifests/extra/yuicompressor/purge.pp
@@ -1,4 +1,8 @@
 class php::extra::yuicompressor::purge
 {
-	file {'/usr/bin/yuicompressor': ensure => absent}
+	file {[
+		'/usr/bin/yuicompressor',
+		'/usr/bin/yuicompressor.jar',
+		'/etc/profile.d/yuicompressor.sh',
+	]: ensure => absent}
 }
