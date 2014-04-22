6b135f534bb0e1f0ea54fb48098ef0c63c91f858
Leonardo Thibes <eu@leonardothibes.com>
2014-04-22 14:24:35 -0300

    instalando o PHPUnit via PHAR, de novo #34

--- manifests/extra/phpunit/install.pp
+++ manifests/extra/phpunit/install.pp
@@ -1,77 +1,8 @@
-class php::extra::phpunit::install inherits php::extra::pear
+class php::extra::phpunit::install inherits php::dependencies
 {
-	$packages = 'php::extra::phpunit::dependencies::packages'
-	if !defined(Package[$packages]) {
-		package {$packages:
-			ensure => present,
-			name   => ['curl','php5-curl'],
-		}
-	}
-	exec {'php::extra::phpunit::step-1':
-		command => 'pear install pear.phpunit.de/PHPUnit',
-        path    => '/usr/bin',
-        onlyif  => 'test ! -f /usr/bin/phpunit',
-		before  => Exec['php::extra::phpunit::step-2'],
-		require => Class['php::extra::pear'],
-	}
-	exec {'php::extra::phpunit::step-2':
-		command => 'pear install pear.phpunit.de/PHPUnit_Selenium',
-		path    => '/usr/bin',
-		onlyif  => 'test ! -f /usr/share/php/PHPUnit/Extensions/SeleniumTestCase.php',
-		before  => Exec['php::extra::phpunit::step-3'],
-		require => [
-			Class['php::extra::pear'],
-			Exec['php::extra::phpunit::step-1'],
-			Package[$packages],
-		],
-	}
-	exec {'php::extra::phpunit::step-3':
-		command => 'pear install pear.phpunit.de/DbUnit',
-		path    => '/usr/bin',
-		onlyif  => 'test ! -f /usr/bin/dbunit',
-		before  => Exec['php::extra::phpunit::step-4'],
-		require => [
-			Class['php::extra::pear'],
-			Exec['php::extra::phpunit::step-2'],
-		],
-	}
-	exec {'php::extra::phpunit::step-4':
-		command => 'pear install pear.phpunit.de/PHP_Invoker',
-		path    => '/usr/bin',
-		onlyif  => 'test ! -f /usr/share/php/PHP/Invoker.php',
-		before  => Exec['php::extra::phpunit::step-5'],
-		require => [
-			Class['php::extra::pear'],
-			Exec['php::extra::phpunit::step-3'],
-		],
-	}
-	exec {'php::extra::phpunit::step-5':
-		command => 'pear install pear.phpunit.de/PHPUnit_Story',
-		path    => '/usr/bin',
-		onlyif  => 'test ! -d /usr/share/php/PHPUnit/Extensions/Story',
-		before  => Exec['php::extra::phpunit::step-6'],
-		require => [
-			Class['php::extra::pear'],
-			Exec['php::extra::phpunit::step-4'],
-		],
-	}
-	exec {'php::extra::phpunit::step-6':
-		command => 'pear install pear.phpunit.de/PHPUnit_SkeletonGenerator',
-		path    => '/usr/bin',
-		onlyif  => 'test ! -d /usr/share/php/SebastianBergmann/PHPUnit/SkeletonGenerator',
-		before  => Exec['php::extra::phpunit::step-7'],
-		require => [
-			Class['php::extra::pear'],
-			Exec['php::extra::phpunit::step-5'],
-		],
-	}
-	exec {'php::extra::phpunit::step-7':
-		command => 'pear install pear.phpunit.de/Diff',
-		path    => '/usr/bin',
-		onlyif  => 'test ! -d /usr/share/php/SebastianBergmann/Diff',
-		require => [
-			Class['php::extra::pear'],
-			Exec['php::extra::phpunit::step-6'],
-		],
+	wget::fetch {'php::extra::phpunit::install':
+		source      => 'https://phar.phpunit.de/phpunit.phar',
+		destination => '/usr/bin/phpunit',
+		chmod       => 0755,
 	}
 }
